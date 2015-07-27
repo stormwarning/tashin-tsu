e4851d5562c033098b13e5389ccb0d162d66844d
Jeff <rustyangel@gmail.com>
2015-07-27 11:35:47 -0600

    Add WordPress `$term` object

---
+++ wordpress/get_queried_object/term.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * get_queried_object( $term ).
+ *
+ * @since 4.2.2
+ */
+
+$term = (object) array(
+  'term_id' =>
+  'name' =>
+  'slug' =>
+  'term_group' =>
+  'term_taxonomy_id' =>
+  'taxonomy' =>
+  'description' =>
+  'parent' =>
+  'count' =>
+  'filter' =>
+);
