af9b76bad01244d06947a2c3077bb6f1140239f9
Jeff <rustyangel@gmail.com>
2015-07-27 11:36:05 -0600

    Add WordPress `$post` object

---
+++ wordpress/get_queried_object/post.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * get_queried_object( $post ).
+ *
+ * @since 4.2.2
+ */
+
+$post = (object) array(
+  'ID' => (integer),
+  'post_author' => (integer),
+  'post_date' => (string),
+  'post_date_gmt' => (string),
+  'post_content' => (string),
+  'post_title' => (string),
+  'post_excerpt' => (string),
+  'post_status' => (string),
+  'comment_status' => (string),
+  'ping_status' => (string),
+  'post_password' => (string),
+  'post_name' => (string),
+  'to_ping' => (string),
+  'pinged' => (string),
+  'post_modified' => (string),
+  'post_modified_gmt' => (string),
+  'post_content_filtered' => ,
+  'post_parent' => (integer),
+  'guid' => (string),
+  'menu_order' => (integer),
+  'post_type' => (string),
+  'post_mime_type' => (string),
+  'comment_count' => (integer),
+  'ancestors' => (array),
+  'filter' => (string),
+);
